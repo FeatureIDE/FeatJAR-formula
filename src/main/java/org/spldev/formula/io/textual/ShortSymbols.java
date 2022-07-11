32f5f68622431e7599a29607e4bbfea31319f967
Elias Kuiter <info@elias-kuiter.de>
2022-07-11 13:39:02 +0200

    Updated license headers

--- src/main/java/org/spldev/formula/io/textual/ShortSymbols.java
+++ src/main/java/org/spldev/formula/io/textual/ShortSymbols.java
@@ -1,23 +1,23 @@
 /* -----------------------------------------------------------------------------
- * Formula Lib - Library to represent and edit propositional formulas.
- * Copyright (C) 2021-2022  Sebastian Krieter
+ * formula - Propositional and first-order formulas
+ * Copyright (C) 2020 Sebastian Krieter
  * 
- * This file is part of Formula Lib.
+ * This file is part of formula.
  * 
- * Formula Lib is free software: you can redistribute it and/or modify it
+ * formula is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
+ * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  * 
- * Formula Lib is distributed in the hope that it will be useful,
+ * formula is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Formula Lib.  If not, see <https://www.gnu.org/licenses/>.
+ * along with formula. If not, see <https://www.gnu.org/licenses/>.
  * 
- * See <https://github.com/skrieter/formula> for further information.
+ * See <https://github.com/FeatJAR/formula> for further information.
  * -----------------------------------------------------------------------------
  */
 package org.spldev.formula.io.textual;
