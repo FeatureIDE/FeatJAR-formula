688273cb6e8f624f476f93c9f708d6ffd36a7fcb
Elias Kuiter <info@elias-kuiter.de>
2023-01-10 13:22:04 +0100

    License

--- src/main/java/de/featjar/formula/analysis/Solvers.java
+++ src/main/java/de/featjar/formula/analysis/Solvers.java
@@ -1,22 +1,22 @@
 /*
- * Copyright (C) 2022 Elias Kuiter
+ * Copyright (C) 2023 Sebastian Krieter, Elias Kuiter
  *
- * This file is part of cli.
+ * This file is part of FeatJAR-formula.
  *
- * cli is free software: you can redistribute it and/or modify it
+ * formula is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  *
- * cli is distributed in the hope that it will be useful,
+ * formula is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with cli. If not, see <https://www.gnu.org/licenses/>.
+ * along with formula. If not, see <https://www.gnu.org/licenses/>.
  *
- * See <https://github.com/FeatureIDE/FeatJAR-cli> for further information.
+ * See <https://github.com/FeatureIDE/FeatJAR-formula> for further information.
  */
 package de.featjar.formula.analysis;
 
--- src/main/java/de/featjar/formula/io/textual/LogicalSymbols.java
+++ src/main/java/de/featjar/formula/io/textual/LogicalSymbols.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) 2022 Sebastian Krieter, Elias Kuiter
+ * Copyright (C) 2023 Sebastian Krieter, Elias Kuiter
  *
- * This file is part of formula.
+ * This file is part of FeatJAR-formula.
  *
  * formula is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
