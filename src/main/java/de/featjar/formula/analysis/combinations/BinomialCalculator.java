9f5f0fe4072e23289e317cc0df788caa0f1f811d
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2023-05-08 11:17:47 +0200

    Fixes problems in binomial calculator

--- src/main/java/de/featjar/formula/analysis/combinations/BinomialCalculator.java
+++ src/main/java/de/featjar/formula/analysis/combinations/BinomialCalculator.java
@@ -30,38 +30,82 @@ public class BinomialCalculator {
     private final long[][] binomial;
     private final long[] factorial;
 
-    public BinomialCalculator(int t, int n) {
-        binomial = new long[n + 1][t + 1];
-        factorial = new long[t + 1];
+    private final int maxK, maxN;
+
+    public BinomialCalculator(int maxK, int maxN) {
+        this.maxK = maxK;
+        this.maxN = maxN;
+        binomial = new long[maxN + 1][maxK + 1];
+        factorial = new long[maxK + 1];
     }
 
     public long factorial(int k) {
-        long f = factorial[k];
-        if (f == 0) {
-            f = 1;
-            for (int i = 2; i <= k; i++) {
-                f *= i;
+        synchronized (factorial) {
+            long f = factorial[k];
+            if (f == 0) {
+                f = 1;
+                for (int i = 2; i <= k; i++) {
+                    f *= i;
+                }
+                factorial[k] = f;
             }
-            factorial[k] = f;
+            return f;
         }
-        return f;
+    }
+
+    public long binomial() {
+        return binomial(maxN, maxK);
     }
 
     public long binomial(int n, int k) {
+        synchronized (binomial) {
+            long b = binomial[n][k];
+            if (b == 0) {
+                b = computeBinomial(n, k);
+                binomial[n][k] = b;
+            }
+            return b;
+        }
+    }
+
+    public int[] combination(long index) {
+        final int[] combination = new int[maxK];
+        for (int i = maxK; i > 0; i--) {
+            if (index <= 0) {
+                combination[i - 1] = i - 1;
+            } else {
+                final int p = (int) Math.ceil(Math.pow(index * factorial(i), (1.0 / i)));
+                for (int j = p; j <= maxN; j++) {
+                    if (binomial(j, i) > index) {
+                        combination[i - 1] = j - 1;
+                        index -= binomial(j - 1, i);
+                        break;
+                    }
+                }
+            }
+        }
+        return combination;
+    }
+
+    public long index(int[] c) {
+        long index = 0;
+        for (int i = 0; i < maxK; i++) {
+            index += binomial(c[i], i + 1);
+        }
+        return index;
+    }
+
+    public static long computeBinomial(int n, int k) {
         if (n < k) {
             return 0;
         }
-        long b = binomial[n][k];
-        if (b == 0) {
-            if (k > (n - k)) {
-                k = n - k;
-            }
+        if (k > (n - k)) {
+            k = n - k;
+        }
 
-            b = 1;
-            for (int i = 1, m = n; i <= k; i++, m--) {
-                b = Math.multiplyExact(b, m) / i;
-            }
-            binomial[n][k] = b;
+        long b = k == 0 ? 1 : n;
+        for (int i = 1; i < k; i++) {
+            b = Math.multiplyExact(b, n - i) / (i + 1);
         }
         return b;
     }
