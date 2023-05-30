f3344ef223047781461369ab4071af5c0b549cee
Elias Kuiter <info@elias-kuiter.de>
2023-05-30 12:58:26 +0200

    workaround for compiler error

--- src/main/java/de/featjar/formula/analysis/combinations/LexicographicIterator.java
+++ src/main/java/de/featjar/formula/analysis/combinations/LexicographicIterator.java
@@ -26,69 +26,70 @@ import java.util.stream.StreamSupport;
 /**
  * Combination iterator that iterates of the combinations in lexicographical
  * order.
+ * todo: commented out because it caused a compiler error in another module
  *
  * @author Sebastian Krieter
  */
-public class LexicographicIterator extends ACombinationIterator {
+public class LexicographicIterator { // extends ACombinationIterator {
 
-    public static Stream<int[]> stream(int t, int size) {
-        return StreamSupport.stream(new LexicographicIterator(t, size).spliterator(), false);
-    }
-
-    private final int[] c;
-
-    public LexicographicIterator(int t, int size) {
-        super(t, size);
-        c = new int[t];
-        for (int i = 0; i < (c.length - 1); i++) {
-            c[i] = i;
-        }
-        c[t - 1] = t - 2;
-    }
-
-    @Override
-    protected int[] computeCombination(long index) {
-        int i = t;
-        for (; i > 0; i--) {
-            final int ci = ++c[i - 1];
-            if (ci < ((n - t) + i)) {
-                break;
-            }
-        }
-        if ((i == 0) && (c[i] == ((n - t) + 1))) {
-            return null;
-        }
-
-        for (; i < t; i++) {
-            if (i == 0) {
-                c[i] = 0;
-            } else {
-                c[i] = c[i - 1] + 1;
-            }
-        }
-        return c;
-    }
-
-    @Override
-    protected long nextIndex() {
-        return 0;
-    }
-
-    @Override
-    public long getIndex() {
-        long index = 0;
-        for (int i = 0; i < c.length; i++) {
-            index += binomialCalculator.binomial(c[i], i + 1);
-        }
-        return index;
-    }
-
-    @Override
-    public void reset() {
-        super.reset();
-        for (int i = 0; i < (c.length - 1); i++) {
-            c[i] = i;
-        }
-        c[t - 1] = t - 2;
-    }
+//    public static Stream<int[]> stream(int t, int size) {
+//        return StreamSupport.stream(new LexicographicIterator(t, size).spliterator(), false);
+//    }
+//
+//    private final int[] c;
+//
+//    public LexicographicIterator(int t, int size) {
+//        super(t, size);
+//        c = new int[t];
+//        for (int i = 0; i < (c.length - 1); i++) {
+//            c[i] = i;
+//        }
+//        c[t - 1] = t - 2;
+//    }
+//
+//    @Override
+//    protected int[] computeCombination(long index) {
+//        int i = t;
+//        for (; i > 0; i--) {
+//            final int ci = ++c[i - 1];
+//            if (ci < ((n - t) + i)) {
+//                break;
+//            }
+//        }
+//        if ((i == 0) && (c[i] == ((n - t) + 1))) {
+//            return null;
+//        }
+//
+//        for (; i < t; i++) {
+//            if (i == 0) {
+//                c[i] = 0;
+//            } else {
+//                c[i] = c[i - 1] + 1;
+//            }
+//        }
+//        return c;
+//    }
+//
+//    @Override
+//    protected long nextIndex() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public long getIndex() {
+//        long index = 0;
+//        for (int i = 0; i < c.length; i++) {
+//            index += binomialCalculator.binomial(c[i], i + 1);
+//        }
+//        return index;
+//    }
+//
+//    @Override
+//    public void reset() {
+//        super.reset();
+//        for (int i = 0; i < (c.length - 1); i++) {
+//            c[i] = i;
+//        }
+//        c[t - 1] = t - 2;
+//    }
 }
