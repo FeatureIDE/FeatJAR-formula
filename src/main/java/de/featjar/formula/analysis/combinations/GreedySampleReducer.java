ec76f76cd56b5a20c61cc4e2cd44da579eb31057
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2024-05-29 10:39:56 +0200

    Updates wrong license header
    Signed-off-by Sebastian Krieter <sebastian.krieter@uni-ulm.de>

--- src/main/java/de/featjar/formula/analysis/combinations/GreedySampleReducer.java
+++ src/main/java/de/featjar/formula/analysis/combinations/GreedySampleReducer.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (C) 2024 FeatJAR-Development-Team
  *
- * This file is part of FeatJAR-evaluation-sample-reducer.
+ * This file is part of FeatJAR-formula.
  *
- * evaluation-sample-reducer is free software: you can redistribute it and/or modify it
+ * formula is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  *
- * evaluation-sample-reducer is distributed in the hope that it will be useful,
+ * formula is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with evaluation-sample-reducer. If not, see <https://www.gnu.org/licenses/>.
+ * along with formula. If not, see <https://www.gnu.org/licenses/>.
  *
- * See <https://github.com/FeatJAR> for further information.
+ * See <https://github.com/FeatureIDE/FeatJAR-formula> for further information.
  */
 package de.featjar.formula.analysis.combinations;
 
