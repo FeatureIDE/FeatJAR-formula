4a362da890b9995f8d5f30f1f04b3f90e9e26bff
niclas <niclas.k@mailbox.org>
2025-11-06 19:32:16 +0100

    refactor: changed format names

--- src/main/java/de/featjar/formula/io/textual/BooleanAssignmentString.java
+++ src/main/java/de/featjar/formula/io/textual/BooleanAssignmentString.java
@@ -1,25 +1,45 @@
+/*
+ * Copyright (C) 2025 FeatJAR-Development-Team
+ *
+ * This file is part of FeatJAR-formula.
+ *
+ * formula is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * formula is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with formula. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * See <https://github.com/FeatureIDE/FeatJAR-formula> for further information.
+ */
 package de.featjar.formula.io.textual;
 
 import de.featjar.base.data.Result;
 import de.featjar.base.io.format.IFormat;
 import de.featjar.formula.assignment.BooleanAssignment;
 
-public class BooleanAssignmentString implements IFormat<BooleanAssignment>{
-	@Override
-	public Result<String> serialize(BooleanAssignment booleanAssignment) {		
+public class BooleanAssignmentString implements IFormat<BooleanAssignment> {
+    @Override
+    public Result<String> serialize(BooleanAssignment booleanAssignment) {
         return Result.of(String.valueOf(booleanAssignment));
     }
-	
-	@Override
-	public BooleanAssignmentString getInstance() {
-		return this;
-	}
 
-	@Override
-	public String getName() {
-		return "BooleanAssignment";
-	}
-	
+    @Override
+    public BooleanAssignmentString getInstance() {
+        return this;
+    }
+
+    @Override
+    public String getName() {
+        return "BooleanAssignment";
+    }
+
     @Override
     public boolean supportsWrite() {
         return true;
@@ -29,7 +49,7 @@ public class BooleanAssignmentString implements IFormat<BooleanAssignment>{
     public boolean supportsParse() {
         return false;
     }
-    
+
     @Override
     public String getFileExtension() {
         return "text";
--- src/main/java/de/featjar/formula/io/textual/FormulaStringFormat.java
+++ src/main/java/de/featjar/formula/io/textual/FormulaStringFormat.java
@@ -1,27 +1,51 @@
+/*
+ * Copyright (C) 2025 FeatJAR-Development-Team
+ *
+ * This file is part of FeatJAR-formula.
+ *
+ * formula is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * formula is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with formula. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * See <https://github.com/FeatureIDE/FeatJAR-formula> for further information.
+ */
 package de.featjar.formula.io.textual;
 
 import de.featjar.base.data.Result;
 import de.featjar.base.io.format.IFormat;
-
 import de.featjar.formula.structure.IFormula;
 
-public class StringFormulaFormat implements IFormat<IFormula>{
+/**
+ * writes formulas into a string
+ *
+ * @author Niclas Kleinert
+ */
+public class FormulaStringFormat implements IFormat<IFormula> {
 
-	@Override
-	public Result<String> serialize(IFormula formula) {		
+    @Override
+    public Result<String> serialize(IFormula formula) {
         return Result.of(formula.print());
     }
-	
-	@Override
-	public StringFormulaFormat getInstance() {
-		return this;
-	}
 
-	@Override
-	public String getName() {
-		return "featuremodel";
-	}
-	
+    @Override
+    public FormulaStringFormat getInstance() {
+        return this;
+    }
+
+    @Override
+    public String getName() {
+        return "featuremodel";
+    }
+
     @Override
     public boolean supportsWrite() {
         return true;
@@ -31,7 +55,7 @@ public class StringFormulaFormat implements IFormat<IFormula>{
     public boolean supportsParse() {
         return false;
     }
-    
+
     @Override
     public String getFileExtension() {
         return "text";
